f8432588f5cc1dabc0dd2d95bd1ac6f4d135fc62
Ryan J. Parker <ryan.jos.parker@gmail.com>
2023-01-12 10:41:51 -0800

    add foo and bar checks for 7 and 11

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const fizzbuzz = num => {
         let str = '';
         i % 3 ? null : str += 'fizz';
         i % 5 ? null : str += 'buzz';
+        i % 7 ? null : str += 'foo';
+        i % 11 ? null : str += 'bar';
         arr.push(str || i);
     }
     return(arr.join('\n'));
